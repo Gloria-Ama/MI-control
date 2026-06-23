5e06736bd43cf704d41ffcb864e0ce6957472bed
Gloria-Ama <draforamagloria@gmail.com>
2026-06-23 16:29:36 -0400

    Remove pycache files from git

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -1,19 +1,5 @@
-    import axios from "axios";
-    import AsyncStorage from "@react-native-async-storage/async-storage";
+import axios from "axios";
 
 export const api = axios.create({
     baseURL: "https://mi-control.onrender.com/api",
 });
-    const api = axios.create({
-    baseURL: "https://undamaged-tabloid-tweezers.ngrok-free.dev/api",
-    });
-
-    api.interceptors.request.use(async (config) => {
-    const token = await AsyncStorage.getItem("accessToken");
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-    });
-
-    export { api };
